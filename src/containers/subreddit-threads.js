e123a8c6a46c771be4509a1d3caed6b5d7b92ec1
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-05-19 10:45:01 -0400

    feat(containers/subreddit-threads): Sort thread cards by score

--- src/containers/subreddit-threads.js
+++ src/containers/subreddit-threads.js
@@ -3,18 +3,23 @@ import { List } from 'immutable';
 import { connect } from 'react-redux';
 import { getFieldsOfSubredditThreads } from '../selectors/main';
 import ThreadCard from '../components/thread-card';
+import { comparator } from 'ramda';
 
 const SubredditThreads = ({ params: { subreddit }, threads }) => {
   return (
     <div>
       {
-        threads.map((thread, i) => (
-          <ThreadCard thread={thread} key={i} subreddit={subreddit} />
-        ))
+        threads
+          .sort(compare)
+          .map((thread, i) => (
+            <ThreadCard thread={thread} key={i} subreddit={subreddit} />
+          ))
       }
     </div>
   );
 };
+
+const compare = comparator((a, b) => a.get('score') > b.get('score'));
 
 SubredditThreads.propTypes = {
   threads: PropTypes.instanceOf(List).isRequired,
