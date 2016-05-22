84541aa2b6c36fe9c787142bd81eb650d171f02b
Ramanpreet N <ramanpreetnara@gmail.com>
2016-05-22 00:00:12 -0400

    refactor(sagas,reducers): Do 'fromJS' in reducers (#12)

--- src/reducers/subreddits.js
+++ src/reducers/subreddits.js
@@ -1,29 +1,35 @@
-import { Map, Set } from 'immutable';
+import { Map, Set, fromJS } from 'immutable';
 import { handleActions } from 'redux-actions';
 import { SUBREDDITS, CHILDREN_EXPANDED, THUMBNAIL_EXPANDED } from '../constants';
 
 const subreddits = handleActions({
 
-  [SUBREDDITS.SAVE_DEFAULT]: (state, { payload }) => (
-    state.set('default', payload.subreddits)
-  ),
+  [SUBREDDITS.SAVE_DEFAULT]: (state, { payload }) => {
+    return state.set('default', fromJS(payload.subreddits));
+  },
 
   [SUBREDDITS.SAVE_THREADS]: (state, { payload }) => {
     const { subreddit, threads, filter } = payload;
     const filterSet = filter ? Set.of(filter) : Set();
-    return state.updateIn(['threads', subreddit], indexedUpsert(threads.map(
-      (thread) => thread.set('filters', filterSet)
-    )));
+    return state.updateIn(
+      ['threads', subreddit], indexedUpsert(fromJS(threads).map(
+        (thread) => thread.set('filters', filterSet)
+      ))
+    );
   },
 
   [SUBREDDITS.SAVE_THREAD_COMMENTS]: (state, { payload }) => {
     const { subreddit, thread, comments } = payload;
-    return state.updateIn(['comments', subreddit, thread], indexedUpsert(comments));
+    return state.updateIn(
+      ['comments', subreddit, thread], indexedUpsert(fromJS(comments))
+    );
   },
 
   [SUBREDDITS.SAVE_THREAD_COMMENTS_TO_CACHE]: (state, { payload }) => {
     const { subreddit, thread, comments } = payload;
-    return state.updateIn(['subredditCommentCache', subreddit, thread], indexedUpsert(comments));
+    return state.updateIn(
+      ['subredditCommentCache', subreddit, thread], indexedUpsert(fromJS(comments))
+    );
   },
 
   [SUBREDDITS.TOGGLE_THREAD_COMMENT_EXPAND_CHILDREN]: (state, { payload }) => {
