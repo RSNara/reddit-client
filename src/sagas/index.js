02e0fd0887019696610fc82a1d38f94c5de37985
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-05-21 23:50:18 -0400

    refactor(sagas,reducers): Do 'fromJS' in reducers

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,7 +2,6 @@ import { takeEvery } from 'redux-saga';
 import { put } from 'redux-saga/effects';
 import { SUBREDDITS } from '../constants';
 import { expect } from 'chai';
-import { fromJS } from 'immutable';
 import {
   saveDefaultSubreddits,
   saveSubredditThreads,
@@ -26,13 +25,13 @@ export default function* root() {
 function* fetchDefaultSubreddits() {
   const response = yield fetch('/reddit/subreddits');
   const subreddits = yield response.json();
-  yield put(saveDefaultSubreddits(fromJS(subreddits.data.children)));
+  yield put(saveDefaultSubreddits(subreddits.data.children));
 }
 
 function* fetchSubredditThreads({ payload: { subreddit, count, after, filter } }) {
   const response = yield fetch(`/reddit/r/${subreddit}/${filter}?count=${count}&after=${after}`);
   const threads = yield response.json();
-  yield put(saveSubredditThreads(subreddit, fromJS(threads.data.children), filter));
+  yield put(saveSubredditThreads(subreddit, threads.data.children, filter));
   yield put(saveSubredditLastFetchedThreadName(subreddit, last(threads.data.children).data.name, filter));
 }
 
@@ -40,8 +39,8 @@ function* fetchSubredditThreadComments({ payload: { subreddit, thread } }) {
   const response = yield fetch(`/reddit/r/${subreddit}/comments/${thread}`);
   const comments = yield response.json();
   expect(comments).to.have.length(2); // first item is the thread
-  yield put(saveSubredditThreads(subreddit, fromJS(comments[0].data.children), null));
-  yield put(saveSubredditThreadComments(subreddit, thread, fromJS(comments[1].data.children)));
+  yield put(saveSubredditThreads(subreddit, comments[0].data.children, null));
+  yield put(saveSubredditThreadComments(subreddit, thread, comments[1].data.children));
 }
 
 function fetchMoreComments(linkId, children) {
@@ -58,10 +57,10 @@ function fetchMoreComments(linkId, children) {
 
 function* fetchSubredditThreadMoreComments({ payload: { linkId, children, subreddit, thread } }) {
   const moreChildren = yield fetchMoreComments(linkId, children);
-  yield put(saveSubredditThreadCommentsToCache(subreddit, thread, fromJS(createCommentTree(moreChildren))));
+  yield put(saveSubredditThreadCommentsToCache(subreddit, thread, createCommentTree(moreChildren)));
 }
 
 function* fetchSubredditThreadMoreRootComments({ payload: { linkId, children, subreddit, thread } }) {
   const moreChildren = yield fetchMoreComments(linkId, children);
-  yield put(saveSubredditThreadComments(subreddit, thread, fromJS(createCommentTree(moreChildren))));
+  yield put(saveSubredditThreadComments(subreddit, thread, createCommentTree(moreChildren)));
 }
